78497c51f3acb62339e3bec68a9f06f95c546a8c
teme19 <temesgentolcha@gmail.com>
2021-06-17 08:16:55 -0700

    c programming

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,9 @@
 #include <stdio.h>
-
+/**
+*Description: this program prints the string in the puts function 
+*/
 int main(void)
 {
-  puts("\"programming is like building a multilingual puzzle\n");
-  return (0);
+puts("\"programming is like building a multilingual puzzle\n");
+return (0);
 }
--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,7 +1,9 @@
 #include <stdio.h>
-
+/**
+*Description: this program prints string in the printf function
+*/
 int main(void)
 {
-  printf("with proper grammar, but the outcome is a piece of art,\n");
-  return (0);
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
 }
