01c9d936f855c814ba054a78687e25047af0f57b
teme19 <temesgentolcha@gmail.com>
2021-06-30 07:59:18 -0700

    edited

--- 0x04-more_functions_nested_loops/holberton.h
+++ 0x04-more_functions_nested_loops/holberton.h
@@ -3,7 +3,6 @@
 
 /*
  * File: holberton.h
- * Auth: Brennan D Baraban
  * Desc: Header file containing prototypes for all functions
  *       used in the 0x03-more_functions_nested_loops directory.
  */
