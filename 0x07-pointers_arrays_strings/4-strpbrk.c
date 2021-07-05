3c2afc19276ea6ac174fc412649a39153b363fb7
teme19 <temesgentolcha@gmail.com>
2021-07-05 06:38:20 -0700

    pointer and array

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -1,31 +1,30 @@
 #include "holberton.h"
 
 /**
-* _strpbrk - search a string for any of a set of bytes
-* @s: source string
-* @accept: accepted characters
-* Return: the string since the first found accepted character
+* _strpbrk - locates the first occurrence in the string s of any
+* of the bytes in the string accept
+* @s: for the char
+* @accept: paramter for accept
+* Return: pointer to the byte s
 */
+
 char *_strpbrk(char *s, char *accept)
 {
-int a = 0, b;
-while (s[a])
+int i, t, j;
+
+i = 0;
+while (s[i] != '\0')
 {
-b = 0;
-
-while (accept[b])
+t = 0;
+for (j = 0; accept[j] != '\0'; j++)
 {
-if (s[a] == accept[b])
-{
-s += a;
-return (s)
+if (s[i] == accept[j])
+t = 1;
 }
-
-b++;
+j = 0;
+if (t == 1)
+return (s + i);
+i++;
 }
-
-a++;
-}
-
-return ('\0');
+return (0);
 }
