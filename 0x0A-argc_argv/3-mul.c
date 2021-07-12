2df0bb7e983c97ea65412382f90b9364d33d9d05
teme19 <temesgentolcha@gmail.com>
2021-07-12 05:25:29 -0700

    C - argc, argv

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -8,15 +8,15 @@
 * @argv: argument vector
 * Return: Always zero
 */
-int main(int argc, char *argv[])
+int main(int argc,char *argv[])
 {
-int n1 = 0, n2 = 0;
+int n1=0, n2=0;
 
-if (argc == 3)
+if (argc==3)
 {
-n1 = atoi(argv[1]);
-n2 = atoi(argv[2]);
-printf("%d\n", n1 * n2);
+n1=atoi(argv[1]);
+n2=atoi(argv[2]);
+printf("%d\n",n1*n2);
 }
 else
 {
