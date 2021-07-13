406d1571216c3ddfcabd63dc660f7f0f36f05f0a
teme19 <temesgentolcha@gmail.com>
2021-07-13 04:58:25 -0700

    C - malloc, free

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -1,52 +1,43 @@
 #include "holberton.h"
 #include <stdlib.h>
-#include <stdio.h>
 
 /**
-* argstostr - convert the params passed to the program to string
-* @ac: the argument count
-* @av: the argument vector
+* argstostr - Concatenates all arguments of the program into a string;
+*             arguments are separated by a new line in the string.
+* @ac: The number of arguments passed to the program.
+* @av: An array of pointers to the arguments.
 * Return: 0
 */
 char *argstostr(int ac, char **av)
 {
-int ch = 0, i = 0, j = 0, k = 0;
-char *s;
+char *str;
+int arg, byte, index, size = ac;
 
 if (ac == 0 || av == NULL)
 return (NULL);
 
-while (i < ac)
+for (arg = 0; arg < ac; arg++)
 {
-while (av[i][j])
-{
-ch++;
-j++;
+for (byte = 0; av[arg][byte]; byte++)
+size++;
 }
 
-j = 0;
-i++;
-}
-s = malloc((sizeof(char) * ch) + ac + 1);
+str = malloc(sizeof(char) * size + 1);
 
-i = 0;
-while (av[i])
+if (str == NULL)
+return (NULL);
+
+index = 0;
+
+for (arg = 0; arg < ac; arg++)
 {
-while (av[i][j])
-{
-s[k] = av[i][j];
-k++;
-j++;
+for (byte = 0; av[arg][byte]; byte++)
+str[index++] = av[arg][byte];
+
+str[index++] = '\n';
 }
 
-s[k] = '\n';
+str[size] = '\0';
 
-j = 0;
-k++;
-i++;
-}
-
-k++;
-s[k] = '\0';
-return (s);
+return (str);
 }
