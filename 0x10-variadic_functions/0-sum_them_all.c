07b22e0d9a9ee99de168446eee0b8ef626672091
teme19 <temesgentolcha@gmail.com>
2021-07-22 07:56:45 -0700

    C - Variadic functions

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -4,7 +4,7 @@
 /**
 * sum_them_all - Returns the sum of all its paramters.
 * @n: The number of paramters passed to the function.
-* @...: A variable number of paramters to calculate the sum of.
+* @n: A variable number of paramters to calculate the sum of.
 * Return: sum.
 */
 int sum_them_all(const unsigned int n, ...)
